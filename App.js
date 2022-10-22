5211406e9f63922cf7dd23c15a63ad9b020cde16
Lilia Lomaka <llomaka80@gmail.com>
2022-10-22 13:22:23 +0300

    Adding background and fonts

--- App.js
+++ App.js
@@ -1,11 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useCallback } from 'react';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import { useFonts } from 'expo-font'; 
+import * as SplashScreen from 'expo-splash-screen';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter: require('./img/fonts/Inter-Medium.ttf'),
+    'Roboto-Bold': require('./img/fonts/Roboto-Bold.ttf'),
+    'Roboto-Medium': require('./img/fonts/Roboto-Medium.ttf'),
+    'Roboto-Regular': require('./img/fonts/Roboto-Regular.ttf'),
+  });
+
+  useEffect(() => {
+    async function prepare() {
+      await SplashScreen.preventAutoHideAsync();
+    }
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.container} onLayout={onLayoutRootView}>
+      <ImageBackground source={require('./img/bg.webp')} style={styles.image}>
+        <Text style={styles.text}>My new app!</Text>
+        <StatusBar style="auto" />
+      </ImageBackground>
     </View>
   );
 }
@@ -18,8 +47,14 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   text: {
-    color: 'grey',
+    fontFamily: 'Roboto-Medium',
+    color: 'black',
     fontSize: 20,
     padding: 10,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
   },
 });
